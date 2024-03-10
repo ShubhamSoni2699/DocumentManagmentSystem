fb7e2987b785f58cd42b55f61b5318ad6341d35f
Mr.2608 <shubhamsoni2699@gmail.com>
2024-03-10 12:46:18 +0530

    database conection code

--- src/db/index.js
+++ src/db/index.js
@@ -1,1 +1,13 @@
 import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async ()=>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.DB_URL}/${DB_NAME}`);
+        console.log(`Mongodb connected !! DB Host : ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("Mongodb connection error :",error);
+    }
+};
+
+export default connectDB;
