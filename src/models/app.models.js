59bc8ae5dd0c164c7cdce5486054bb8720e9c7f6
Mr.2608 <shubhamsoni2699@gmail.com>
2024-03-13 10:20:06 +0530

    logic for password encryption

--- src/models/app.models.js
+++ src/models/app.models.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const appSchema = new mongoose.Schema(
     {
@@ -13,5 +14,16 @@ const appSchema = new mongoose.Schema(
     },
     {timestamps:true}
 )
+
+appSchema.pre("save", async function (next){
+    if(this.isModified("password")){
+        this.password = bcrypt.hash(this.password , process.env.SALT_ROUND);
+    }
+    next();
+})
+
+appSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password,this.password);
+}
 
 export const App = mongoose.model("App",appSchema)
