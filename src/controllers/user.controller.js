b9d4061fcf69472f9a75eab85d941ee35a4b9d97
Mr.2608 <shubhamsoni2699@gmail.com>
2024-03-15 13:51:05 +0530

    bug fix:- profile pic handling

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,8 @@ const registerUser = asyncHandler( async (req,res) =>{
 
     const {name,email,dob} = req.body;
 
+    
+
     res.status(200).json({
         message:"ok"
     });
--- src/middlewares/profilePicHandling.middleware.js
+++ src/middlewares/profilePicHandling.middleware.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        const {name} = req.body.name;
+        const {name} = req.body;
         cb(null, `./uploads/${name}/img`)
     },
     filename: function (req, file, cb) {
